328595792456b1f0ad702f12d03c92b3b31a7a33
crivas <conytax.cra@gmail.com>
2018-11-12 16:31:55 -0300

    nuevo

--- Celulosa/laravel/resources/views/welcome.blade.php
+++ Celulosa/laravel/resources/views/welcome.blade.php
@@ -13,16 +13,15 @@
     
    }
    header {
-    background-color: #CD5C5C;
+    background-color: #2E9AFE;
     text-orientation: center;
     font-size:30px;
     width: 100%;
     height: 38%;
     font-family: 'Monoton', cursive;
-    background-image:url(imagenes/mm.jpg);
    }
    nav{
-    background-color: #FFE4B5;
+    background-color: #58ACFA;
     width: 100%;
     height: 5%;
     font-family: 'Playfair Display SC', serif;
@@ -30,76 +29,14 @@
     font-family: 'Gochi Hand', cursive;
     font-family: 'Copse', serif;
    }
-   .content-all{
-    width: 210px;
-    margin: auto;
-    perspective: 800px;
-    position: relative;
-    margin-top: 50px;
-}
-
-.content-carrousel{
-    width: 100%;
-    position: absolute;
-    animation: rotar 10s infinite linear;
-    transform-style: preserve-3d;
-}
-
-.content-carrousel:hover{
-    animation-play-state: paused;
-    cursor: pointer;
-}
-
-
-.content-carrousel figure{
-    width: 100%;
-    height: 120px;
-    overflow: hidden;
-    position: absolute;
-    box-shadow: 0px 0px 20px 0px black;
-    transition: all 300ms;
-    
-}
-
-.content-carrousel figure:hover{
-    box-shadow: 0px 0px 0px 0px black;
-    transition: all 300ms;
-}
-   .content-carrousel figure:hover{
-    box-shadow: 0px 0px 0px 0px black;
-    transition: all 300ms;
-}
-
-.content-carrousel figure:nth-child(1){transform: rotateZ(0deg) translateZ(300px);}
-
-
-
-.content-carrousel img{
-    width: 90%;
-    transition: all 300ms;
-}
-
-.content-carrousel img:hover{
-    transform: scale(1.3);
-    transition: all 200ms;
-}
-@keyframes rotar{
-    from{
-        transform: rotateY(0deg);
-    }to{
-        transform: rotateY(360deg);
-    }
-}
-
-
    section{
-    background-color: #FFEFD5;
+    background-color: #2E9AFE;
     width: 100%;
     height: 225%;
    }
    
    footer{
-    background-color: #F5DEB3;
+    background-color: #2E9AFE;
     width: 100%;
     height: 12%;
    }
@@ -111,15 +48,15 @@
     
    }
    #menu li{
-    height: 50px;
-    width: 200px;
+    height: 50px; /* fondo del menu letras  */
+    width: 150px; /* tamaño del fondo letra menu  */
     text-align: center;
     float : left; 
-    margin-right:  100px;
-    line-height: 50px;
-    background-color: #FFE4B5;
+    margin-right:  25px;
+    line-height: 50px; /* corre hace abajo letra del menu */
+    background-color: #FFE4B5;  /* color fono de letra */
     color: white;
-    font-size: 35px;
+    font-size: 25px;
     color: black;
     list-style:none;
    }
@@ -150,12 +87,14 @@
   <nav> 
    <ul id ="menu" >
      <div class="links">
-                    <a href="https://laravel.com/docs">login</a>
-                    <a href="https://laracasts.com">register</a>
-                    <a href="Materias">Materias</a>
-                    <a href="https://nova.laravel.com">Comentarios</a>
-                    <a href="https://forge.laravel.com">Calificacions</a>
-                    <a href="https://github.com/laravel/laravel">Integracion</a>
+                    <li href="https://laravel.com/docs">login </li>
+                    <li href="https://laracasts.com">register</li>
+                    <li href="Materias">Materias</li>
+                    <li href="https://nova.laravel.com">Comentarios</li>
+                    <li href="https://forge.laravel.com">Calificacions</li>
+                    <li href="https://github.com/laravel/laravel">Integracion</li>
                 </div>
             </ul> 
         </nav>
+  <section>
+  </section>
