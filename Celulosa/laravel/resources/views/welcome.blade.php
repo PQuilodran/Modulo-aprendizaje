4aaa5f5213cbcd8d5f09ee1a9ef03438e25fbf28
crivas <conytax.cra@gmail.com>
2018-11-12 12:30:21 -0300

    edicionpp

--- Celulosa/laravel/resources/views/welcome.blade.php
+++ Celulosa/laravel/resources/views/welcome.blade.php
@@ -1,90 +1,155 @@
-<!doctype html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<!DOCTYPE html>
+<html len="es">
+ <head>
+  <title>Celulosa</title>
+  <meta charset="UTF-8"/>
+  <link href="https://fonts.googleapis.com/css?family=Monoton" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Copse|Gochi+Hand|Handlee|Playfair+Display+SC" rel="stylesheet">
+  <style>
+   body {
+    margin: 0px;
+    width: 100%;
+    height: 1000px;
+    
+   }
+   header {
+    background-color: #CD5C5C;
+    text-orientation: center;
+    font-size:30px;
+    width: 100%;
+    height: 38%;
+    font-family: 'Monoton', cursive;
+    background-image:url(imagenes/mm.jpg);
+   }
+   nav{
+    background-color: #FFE4B5;
+    width: 100%;
+    height: 5%;
+    font-family: 'Playfair Display SC', serif;
+    font-family: 'Handlee', cursive;
+    font-family: 'Gochi Hand', cursive;
+    font-family: 'Copse', serif;
+   }
+   .content-all{
+    width: 210px;
+    margin: auto;
+    perspective: 800px;
+    position: relative;
+    margin-top: 50px;
+}
 
-        <title>Laravel</title>
+.content-carrousel{
+    width: 100%;
+    position: absolute;
+    animation: rotar 10s infinite linear;
+    transform-style: preserve-3d;
+}
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
+.content-carrousel:hover{
+    animation-play-state: paused;
+    cursor: pointer;
+}
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
 
-            .full-height {
-                height: 100vh;
-            }
+.content-carrousel figure{
+    width: 100%;
+    height: 120px;
+    overflow: hidden;
+    position: absolute;
+    box-shadow: 0px 0px 20px 0px black;
+    transition: all 300ms;
+    
+}
 
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
+.content-carrousel figure:hover{
+    box-shadow: 0px 0px 0px 0px black;
+    transition: all 300ms;
+}
+   .content-carrousel figure:hover{
+    box-shadow: 0px 0px 0px 0px black;
+    transition: all 300ms;
+}
 
-            .position-ref {
-                position: relative;
-            }
+.content-carrousel figure:nth-child(1){transform: rotateZ(0deg) translateZ(300px);}
 
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
 
-            .content {
-                text-align: center;
-            }
 
-            .title {
-                font-size: 84px;
-            }
+.content-carrousel img{
+    width: 90%;
+    transition: all 300ms;
+}
 
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
+.content-carrousel img:hover{
+    transform: scale(1.3);
+    transition: all 200ms;
+}
+@keyframes rotar{
+    from{
+        transform: rotateY(0deg);
+    }to{
+        transform: rotateY(360deg);
+    }
+}
 
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
 
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                     Integracion Bitacora
-                </div>
-
-                <div class="links">
+   section{
+    background-color: #FFEFD5;
+    width: 100%;
+    height: 225%;
+   }
+   
+   footer{
+    background-color: #F5DEB3;
+    width: 100%;
+    height: 12%;
+   }
+   
+   #menu{
+    margin: 0px;
+    position:inherit;
+    height: 100%;
+    
+   }
+   #menu li{
+    height: 50px;
+    width: 200px;
+    text-align: center;
+    float : left; 
+    margin-right:  100px;
+    line-height: 50px;
+    background-color: #FFE4B5;
+    color: white;
+    font-size: 35px;
+    color: black;
+    list-style:none;
+   }
+   h1{
+    margin:  0px;
+   }
+   #contacto{
+    margin-top: 0px;
+   }
+   #contacto li{
+    list-style:none;
+   }
+   p{
+    font-size: 30px;
+    text-align: center;
+   }
+   </style>
+ </head>
+ <body>
+  <header>
+                <br>
+                <br>
+                <br>
+           <center><h1 style ="text-align:center ">Celulosa</h1></center>
+                      
+  
+        </header>
+  <nav> 
+   <ul id ="menu" >
+     <div class="links">
                     <a href="https://laravel.com/docs">login</a>
                     <a href="https://laracasts.com">register</a>
                     <a href="Materias">Materias</a>
@@ -92,7 +157,5 @@
                     <a href="https://forge.laravel.com">Calificacions</a>
                     <a href="https://github.com/laravel/laravel">Integracion</a>
                 </div>
-            </div>
-        </div>
-    </body>
-</html>
+            </ul> 
+        </nav>
