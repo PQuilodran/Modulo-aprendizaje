8cf29365d08922f91b982117e95ef57662be3c96
crivas <conytax.cra@gmail.com>
2018-11-12 19:51:06 -0300

    principal1

---
+++ Celulosa/laravel/resources/views/principal.php
@@ -0,0 +1,100 @@
+<!DOCTYPE html>
+<html len="es">
+ <head>
+  <title>Celulosa</title>
+  <meta charset="UTF-8"/>
+  <link href="https://fonts.googleapis.com/css?family=Monoton" rel="stylesheet"> 
+  <style>
+   body {
+    margin: 0px;
+    width: 100%;
+    height: 1000px;
+   }
+   header {
+    background-color: #2E9AFE;
+    text-orientation: center;
+    font-size:30px;
+    width: 100%;
+    height: 38%;
+    font-family: 'Monoton', cursive;
+   }
+   nav{
+    background-color: #58ACFA;
+    width: 100%;
+    height: 5%;
+    font-family: 'Playfair Display SC', serif;
+    font-family: 'Handlee', cursive;
+    font-family: 'Gochi Hand', cursive;
+    font-family: 'Copse', serif;
+   }
+   section{
+    background-color: #2E9AFE;
+    width: 100%;
+    height: 225%;
+   }
+   
+   footer{
+    background-color: #2E9AFE;
+    width: 100%;
+    height: 12%;
+   }
+   
+   #menu{
+    margin: 0px;
+    position:inherit;
+    height: 100%;
+    
+   }
+   #menu li{
+    height: 50px; /* fondo del menu letras  */
+    width: 150px; /* tamaño del fondo letra menu  */
+    text-align: center;
+    float : left; 
+    margin-right:  25px;
+    line-height: 50px; /* corre hace abajo letra del menu */
+    background-color: #FFE4B5;  /* color fono de letra */
+    color: white;
+    font-size: 25px;
+    color: black;
+    list-style:none;
+   }
+   h1{
+    margin:  0px;
+   }
+   #contacto{
+    margin-top: 0px;
+   }
+   #contacto li{
+    list-style:none;
+   }
+   p{
+    font-size: 30px;
+    text-align: center;
+   }
+   </style>
+ </head>
+ <body>
+  <header>
+                <br>
+                <br>
+                <br>
+           <li Style="font-size:30px"    href="https://laravel.com/docs">login </li>
+           <li href="https://laracasts.com">register</li>
+           
+           <center><h1 style ="text-align:center ">Celulosa</h1></center>
+           
+                      
+  
+        </header>
+  <nav> 
+   <ul id ="menu" >
+     <div class="links">
+                    
+                    <li href="Materias">Materias</li>
+                    <li href="Comentarios">Comentarios</li>
+                    <li href="calificaciones">Calificacions</li>
+                </div>
+            </ul> 
+        </nav>
+  <section>
+  </section>
