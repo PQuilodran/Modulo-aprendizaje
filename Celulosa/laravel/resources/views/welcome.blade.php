7c106307db159cbcaab048034db310cbe63bc376
Paula Quilodran <pquilodran2017@alu.uct.cl>
2018-11-12 11:49:11 -0300

    avance

--- Celulosa/laravel/resources/views/welcome.blade.php
+++ Celulosa/laravel/resources/views/welcome.blade.php
@@ -81,15 +81,15 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                     Integracion Bitacora
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
+                    <a href="https://laravel.com/docs">login</a>
+                    <a href="https://laracasts.com">register</a>
+                    <a href="https://laravel-news.com">Materias</a>
+                    <a href="https://nova.laravel.com">Comentarios</a>
+                    <a href="https://forge.laravel.com">Calificacions</a>
                     <a href="https://github.com/laravel/laravel">GitHub</a>
                 </div>
             </div>
